0a0450551a392cc7cc5ae257cddece04d00df552
Lex <domian844@gmail.com>
2020-07-02 03:05:15 +0700

    RC1.0 - add tooltip size

--- src/main/java/chestup/ItemIronChest.java
+++ src/main/java/chestup/ItemIronChest.java
@@ -40,6 +40,38 @@ public class ItemIronChest extends ItemBlock {
     @Override
     public void addInformation(ItemStack item, EntityPlayer par2, List aList, boolean b) {
         super.addInformation(item, par2, aList, b);
+        switch (item.getItemDamage()) {
+            case 0:
+                aList.add("Size: " + EnumChatFormatting.YELLOW + "45");
+                break;
+            case 1:
+                aList.add("Size: " + EnumChatFormatting.YELLOW + "63");
+                break;
+            case 2:
+                aList.add("Size: " + EnumChatFormatting.YELLOW + "81");
+                break;
+            case 3:
+                aList.add("Size: " + EnumChatFormatting.YELLOW + "99");
+                break;
+            case 4:
+                aList.add("Size: " + EnumChatFormatting.YELLOW + "117");
+                break;
+            case 5:
+                aList.add("Size: " + EnumChatFormatting.YELLOW + "135");
+                break;
+            case 6:
+                aList.add("Size: " + EnumChatFormatting.YELLOW + "153");
+                break;
+            case 7:
+                aList.add("Size: " + EnumChatFormatting.YELLOW + "171");
+                break;
+            case 8:
+                aList.add("Size: " + EnumChatFormatting.YELLOW + "189");
+                break;
+            case 9:
+                aList.add("Size: " + EnumChatFormatting.YELLOW + "207");
+                break;
+        }
         aList.add("Author: " + EnumChatFormatting.YELLOW + "4gname");
     }
 }
